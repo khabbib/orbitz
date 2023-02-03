a23791f43a4de8b3dd9eb31f4b7cc0d0e3c0b46a
Axel nilsson <axel.nilsson01@gmail.com>
2023-02-03 11:18:31 +0100

    test

---
+++ src/test/java/Controller/Calculators/PositionCalculatorTestTest.java
@@ -0,0 +1,12 @@
+package Controller.Calculators;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PositionCalculatorTestTest {
+
+    @Test
+    void setDay() {
+    }
+}
