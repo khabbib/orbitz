b88602231f96dfd63597b970bf5f8d477fd02564
khabib <habib.pakdel1121@gmail.com>
2023-02-17 23:28:39 +0100

    if statement to switch, clearn up return statements

--- src/main/java/Controller/Calculators/PositionCalculator.java
+++ src/main/java/Controller/Calculators/PositionCalculator.java
@@ -26,11 +26,11 @@ public class PositionCalculator {
     /**
      * Calculates and sets a value for a planets position
      *
-     * @param d      the variable that represents the specific date
-     * @param planet Planet object to check which planet it is
+     * @param date      the variable that represents the specific date
+     * @param planetName Planet object to check which planet it is
      * @return angle (true anomaly + argument of perihelion + 90 or 180)
      */
-    public double getValues(double d, String planet) {
+    public double getValues(double date, String planetName) {
         double capitalOmega;
         double inclination;
         double smallOmega;
@@ -43,75 +43,74 @@ public class PositionCalculator {
         double heliocentricDistance;
         double trueAnomaly;
 
-        if (planet.equals("Mercury")) {
-            capitalOmega = 48.3313 + 3.24587E-5 * d;
-            inclination = 7.0047 + 5.00E-8 * d;
-            smallOmega = 29.1241 + 1.01444E-5 * d;
-            semiMajorAxis = 0.387098;
-            eccentricity = 0.205635 + 5.59E-10 * d;
-            meanAnomaly = 168.6562 + 4.0923344368 * d;
+        switch (planetName) {
+            case "Mercury":
+                capitalOmega = 48.3313 + 3.24587E-5 * date;
+                inclination = 7.0047 + 5.00E-8 * date;
+                smallOmega = 29.1241 + 1.01444E-5 * date;
+                semiMajorAxis = 0.387098;
+                eccentricity = 0.205635 + 5.59E-10 * date;
+                meanAnomaly = 168.6562 + 4.0923344368 * date;
+                break;
+            case "Venus":
+                capitalOmega = 76.6799 + 2.46590E-5 * date;
+                inclination = 3.3946 + 2.75E-8 * date;
+                smallOmega = 54.8910 + 1.38374E-5 * date;
+                semiMajorAxis = 0.723330;
+                eccentricity = 0.006773 - 1.302E-9 * date;
+                meanAnomaly = 48.0052 + 1.6021302244 * date;
+                break;
+            case "Earth":
+                smallOmega = 282.9404 + 4.70935E-5 * date;
+                semiMajorAxis = 1.000000;
+                eccentricity = 0.016709 - 1.151E-9 * date;
+                meanAnomaly = 356.0470 + 0.9856002585 * date;
+                break;
+            case "Mars":
+                capitalOmega = 49.5574 + 2.11081E-5 * date;
+                inclination = 1.8497 - 1.78E-8 * date;
+                smallOmega = 286.5016 + 2.92961E-5 * date;
+                semiMajorAxis = 1.523688;
+                eccentricity = 0.093405 + 2.516E-9 * date;
+                meanAnomaly = 18.6021 + 0.5240207766 * date;
+                break;
+            case "Jupiter":
+                capitalOmega = 100.4542 + 2.76854E-5 * date;
+                inclination = 1.3030 - 1.557E-7 * date;
+                smallOmega = 273.8777 + 1.64505E-5 * date;
+                semiMajorAxis = 5.20256;
+                eccentricity = 0.048498 + 4.469E-9 * date;
+                meanAnomaly = 19.8950 + 0.0830853001 * date;
+                break;
+            case "Saturn":
+                capitalOmega = 113.6634 + 2.38980E-5 * date;
+                inclination = 2.4886 - 1.081E-7 * date;
+                smallOmega = 339.3939 + 2.97661E-5 * date;
+                semiMajorAxis = 9.55475;
+                eccentricity = 0.055546 - 9.499E-9 * date;
+                meanAnomaly = 316.9670 + 0.0334442282 * date;
+                break;
+            case "Uranus":
+                capitalOmega = 74.0005 + 1.3978E-5 * date;
+                inclination = 0.7733 + 1.9E-8 * date;
+                smallOmega = 96.6612 + 3.0565E-5 * date;
+                semiMajorAxis = 19.18171 - 1.55E-8 * date;
+                eccentricity = 0.047318 + 7.45E-9 * date;
+                meanAnomaly = 142.5905 + 0.011725806 * date;
+                break;
+            case "Neptune":
+                capitalOmega = 131.7806 + 3.0173E-5 * date;
+                inclination = 1.7700 - 2.55E-7 * date;
+                smallOmega = 272.8461 - 6.027E-6 * date;
+                semiMajorAxis = 30.05826 + 3.313E-8 * date;
+                eccentricity = 0.008606 + 2.15E-9 * date;
+                meanAnomaly = 260.2471 + 0.005995147 * date;
+                break;
+            default:
+                System.out.println("Not valid planet name.");
+                return -1;
         }
 
-        if (planet.equals("Venus")) {
-            capitalOmega = 76.6799 + 2.46590E-5 * d;
-            inclination = 3.3946 + 2.75E-8 * d;
-            smallOmega = 54.8910 + 1.38374E-5 * d;
-            semiMajorAxis = 0.723330;
-            eccentricity = 0.006773 - 1.302E-9 * d;
-            meanAnomaly = 48.0052 + 1.6021302244 * d;
-        }
-
-        if (planet.equals("Earth")) {
-            smallOmega = 282.9404 + 4.70935E-5 * d;
-            semiMajorAxis = 1.000000;
-            eccentricity = 0.016709 - 1.151E-9 * d;
-            meanAnomaly = 356.0470 + 0.9856002585 * d;
-        }
-
-        if (planet.equals("Mars")) {
-            capitalOmega = 49.5574 + 2.11081E-5 * d;
-            inclination = 1.8497 - 1.78E-8 * d;
-            smallOmega = 286.5016 + 2.92961E-5 * d;
-            semiMajorAxis = 1.523688;
-            eccentricity = 0.093405 + 2.516E-9 * d;
-            meanAnomaly = 18.6021 + 0.5240207766 * d;
-        }
-
-        if (planet.equals("Jupiter")) {
-            capitalOmega = 100.4542 + 2.76854E-5 * d;
-            inclination = 1.3030 - 1.557E-7 * d;
-            smallOmega = 273.8777 + 1.64505E-5 * d;
-            semiMajorAxis = 5.20256;
-            eccentricity = 0.048498 + 4.469E-9 * d;
-            meanAnomaly = 19.8950 + 0.0830853001 * d;
-        }
-
-        if (planet.equals("Saturn")) {
-            capitalOmega = 113.6634 + 2.38980E-5 * d;
-            inclination = 2.4886 - 1.081E-7 * d;
-            smallOmega = 339.3939 + 2.97661E-5 * d;
-            semiMajorAxis = 9.55475;
-            eccentricity = 0.055546 - 9.499E-9 * d;
-            meanAnomaly = 316.9670 + 0.0334442282 * d;
-        }
-
-        if (planet.equals("Uranus")) {
-            capitalOmega = 74.0005 + 1.3978E-5 * d;
-            inclination = 0.7733 + 1.9E-8 * d;
-            smallOmega = 96.6612 + 3.0565E-5 * d;
-            semiMajorAxis = 19.18171 - 1.55E-8 * d;
-            eccentricity = 0.047318 + 7.45E-9 * d;
-            meanAnomaly = 142.5905 + 0.011725806 * d;
-        }
-
-        if (planet.equals("Neptune")) {
-            capitalOmega = 131.7806 + 3.0173E-5 * d;
-            inclination = 1.7700 - 2.55E-7 * d;
-            smallOmega = 272.8461 - 6.027E-6 * d;
-            semiMajorAxis = 30.05826 + 3.313E-8 * d;
-            eccentricity = 0.008606 + 2.15E-9 * d;
-            meanAnomaly = 260.2471 + 0.005995147 * d;
-        }
 
         eccentricAnomalyApproximation = meanAnomaly + (180 / Math.PI) * eccentricity * Math.sin(Math.PI / 180 * meanAnomaly) * (1 + (eccentricity * Math.cos(Math.PI / 180 * meanAnomaly)));
 
@@ -120,21 +119,23 @@ public class PositionCalculator {
         rectangularCoordinateX = calcX(semiMajorAxis, eccentricAnomaly, eccentricity);
         rectangularCoordinateY = calcY(semiMajorAxis, eccentricAnomaly, eccentricity);
 
+        // Variable is not used in the code
         heliocentricDistance = Math.sqrt((rectangularCoordinateX * rectangularCoordinateX) + (rectangularCoordinateY * rectangularCoordinateY));
         trueAnomaly = Math.toDegrees(Math.atan2(rectangularCoordinateY, rectangularCoordinateX));
 
-        if (planet.equals("Earth")) {
-            return (trueAnomaly + smallOmega) + 180;
-        } else if (planet.equals("Venus")) {
+
+        if (planetName.equals("Earth")) {
             return (trueAnomaly + smallOmega) + 180;
         }
-
-        if (planet.equals("Mercury")) {
-
+        if (planetName.equals("Venus")) {
+            return (trueAnomaly + smallOmega) + 180;
+        }
+        if (planetName.equals("Mercury")) {
             return (trueAnomaly + smallOmega) + 90;
-        } else {
-            return trueAnomaly + smallOmega;
         }
+
+        return trueAnomaly + smallOmega;
+
     }
 
     public double calcY(double a, double E1, double e) {
