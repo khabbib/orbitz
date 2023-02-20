3cb8f7f8b1cd56dace7cee7edaf56bb90e84cb16
Kasper Kadestål <84015423+Kasperkadestal@users.noreply.github.com>
2023-02-20 19:34:07 +0100

    1.1

---
+++ src/test/java/Model/OrbitTest.java
@@ -0,0 +1,32 @@
+package Model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OrbitTest {
+
+    @Test
+    void getWidth() {
+        Orbit orbit = new Orbit(100,100,100,100);
+        assertEquals(10,orbit.getWidth(10));
+    }
+
+    @Test
+    void getHeight() {
+        Orbit orbit = new Orbit(100,100,100,100);
+        assertEquals(10,orbit.getHeight(10));
+    }
+
+    @Test
+    void getCenterXCord() {
+        Orbit orbit = new Orbit(100,100,100,100);
+        assertEquals(10,orbit.getCenterXCord(10));
+    }
+
+    @Test
+    void getCenterYCord() {
+        Orbit orbit = new Orbit(100,100,100,100);
+        assertEquals(10,orbit.getCenterYCord(10));
+    }
+}
