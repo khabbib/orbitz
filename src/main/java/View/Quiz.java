ec12120d5dd7ef827a0c9eea778464f7354d8de0
khabib <habib.pakdel1121@gmail.com>
2023-02-23 22:39:18 +0100

    quiz window and close button

--- src/main/java/View/Quiz.java
+++ src/main/java/View/Quiz.java
@@ -1,4 +1,24 @@
 package View;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
 public class Quiz {
+    @FXML
+    private Button closeQuiz;
+
+
+    public void initialize() {
+
+
+
+
+
+        /**
+         * Close button - return to orbit scene
+         */
+        closeQuiz.setOnAction(e -> {
+            MainFrame.changeScene("Orbit");
+        });
+    }
 }
