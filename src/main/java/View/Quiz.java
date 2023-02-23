9eff5a3eb578ec8798c215191d252c833cbee397
khabib <habib.pakdel1121@gmail.com>
2023-02-23 23:23:26 +0100

    added question panel

--- src/main/java/View/Quiz.java
+++ src/main/java/View/Quiz.java
@@ -2,11 +2,34 @@ package View;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.text.Text;
 
 public class Quiz {
     @FXML
     private Button closeQuiz;
+    @FXML
+    private Text question;
 
+
+    // Planets
+    @FXML
+    private Button neptune;
+    @FXML
+    private Button uranus;
+    @FXML
+    private Button saturn;
+    @FXML
+    private Button jupiter;
+    @FXML
+    private Button mars;
+    @FXML
+    private Button earth;
+    @FXML
+    private Button venus;
+    @FXML
+    private Button mercury;
+    @FXML
+    private Button sun;
 
     public void initialize() {
 
