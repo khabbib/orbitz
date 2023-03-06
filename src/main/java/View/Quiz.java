d67e1beb39adac362ac9500bcb98a696ec6532f2
Axel nilsson <axel.nilsson01@gmail.com>
2023-03-06 11:47:13 +0100

    Update Quiz.java

--- src/main/java/View/Quiz.java
+++ src/main/java/View/Quiz.java
@@ -18,6 +18,7 @@ import javafx.util.Duration;
 
 import java.net.URL;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.ResourceBundle;
 import java.util.concurrent.atomic.AtomicInteger;
@@ -36,7 +37,7 @@ public class Quiz implements Initializable {
     @FXML
     private ImageView confetti;
 
-    HashMap<String, String> userAnswers = new HashMap<>();
+    HashMap<String, String> userAnswers = new LinkedHashMap<>();
 
     @FXML
     private AnchorPane planetScreen;
